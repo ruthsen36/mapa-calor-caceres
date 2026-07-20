a3f72226729a52c256617d72a5974ba303005f81
William <ruthsen36@gmail.com>
2026-07-20 14:03:51 -0400

    Add WebAPK native app icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'caceres-99-heatmap-v7';
+const CACHE_NAME = 'caceres-99-heatmap-v8';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -6,13 +6,14 @@ const ASSETS_TO_CACHE = [
   './app.js',
   './caceres-data.js',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
   'https://unpkg.com/leaflet.heat@0.2.0/dist/leaflet-heat.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
 ];
 
-// Instalação do Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -23,7 +24,6 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-// Ativação e Limpeza de caches antigos
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keyList) => {
@@ -40,7 +40,6 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-// Intercepta requisições de rede para funcionamento Offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
